475993e0507170b72f4a82f2465162957ab74ad1
mehdi <mehdi.rabbai@gmail.com>
2024-01-29 22:41:54 +0100

    Add Convex for real time database and Clerk for authentification

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      <Button size="sm" variant="secondary">
-        Hello Board
-      </Button>
-    </>
+    <div className="flex flex-col gap-y-4">
+      <div>This is a screen for Authenticated users only</div>
+      <div>
+        <UserButton />
+      </div>
+    </div>
   );
 }
